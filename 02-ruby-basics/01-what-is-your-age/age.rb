9d177ee7a7ccf99234b8f4cee9bb78ea2d143768
Cassandre <cassandreporretti@gmail.com>
2019-11-06 14:34:29 +0000

    Add age calculus

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -1,6 +1,9 @@
 puts "What is your name?"
 name = gets.chomp # asks user to type something in the Terminal
 
-puts "Ok got it!"
-
 # TODO: your code goes here
+puts "What is your year of birth?"
+birth_year=gets.chomp.to_i
+age=2019-birth_year
+
+puts "Hey #{name}! You are #{age} years old."
