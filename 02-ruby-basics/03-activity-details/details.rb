76d97176364d72cf2080a9f6e3f9f165518d797d
Cassandre <cassandreporretti@gmail.com>
2019-11-06 15:18:55 +0000

    creation first activity

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,8 @@
-activity = {
-  # TODO: gather the characteristics of your preferred activity
-}
+activity = {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Paris" , "duration" => 60 , "number_participants" => 6 , "price" => 120}
 
 # TODO: your code goes here
+puts "#{activity["name"]} [#{activity["category"].upcase}]"
+puts "Located in #{activity["city"]}"
+puts "Duration of #{activity["duration"]} minutes"
+puts "For #{activity["number_participants"]} participants"
+puts "Charged #{activity["price"]} euros"
