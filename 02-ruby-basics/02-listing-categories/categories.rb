97a77cb054f1cfb1d75fb690b0bc85b78927705a
Cassandre <cassandreporretti@gmail.com>
2019-11-06 14:56:13 +0000

    Add new activity choice

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,17 @@
-categories = [
-  # TODO: gather the categories of activity
-]
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
 
 # TODO: your code goes here
+puts "What activity would you like to add?"
+new_activity = gets.chomp
+
+categories = categories << new_activity
+categories_sorted = categories.sort
+
+puts "[CATEGORIES OF ACTIVITIES]"
+categories_sorted.each do |category|
+  puts "-#{category}"
+end
+
+number_categories=categories_sorted.count
+
+puts "Total : #{number_categories}"
