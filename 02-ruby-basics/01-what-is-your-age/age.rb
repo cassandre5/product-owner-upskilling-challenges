61f434e4abfa90b0d8d3b07dd388feaccbbb5f3c
Cassandre <cassandreporretti@gmail.com>
2019-11-06 14:40:33 +0000

    Add calculus whatever current year

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -4,6 +4,7 @@ name = gets.chomp # asks user to type something in the Terminal
 # TODO: your code goes here
 puts "What is your year of birth?"
 birth_year=gets.chomp.to_i
-age=2019-birth_year
+this_year=Time.now.year
+age=this_year-birth_year
 
 puts "Hey #{name}! You are #{age} years old."
