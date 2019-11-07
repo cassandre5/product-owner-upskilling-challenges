5f2f76fcef2e2eb9d92fe274d17182d0e51a74ed
Cassandre <cassandreporretti@gmail.com>
2019-11-07 17:01:26 +0000

    update ruby

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,10 +6,15 @@ enable :static
 
 get "/" do
   @activities = [
-    {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Paris" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "images/escape.jpg"} ,
-    {"name" => "Ping-pong" , "category" => "Sport" , "city" => "Lille" , "duration" => 90 , "number_participants" => 4 , "price" => 80 , "photo" => "images/ping-pong.jpg"},
+    {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Brive" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "images/escape.jpg"} ,
+    {"name" => "Beach volley" , "category" => "Sport" , "city" => "Marseille" , "duration" => 90 , "number_participants" => 4 , "price" => 60 , "photo" => "images/beach-volley.jpg"},
     {"name" => "Museum" , "category" => "Visits" , "city" => "Paris" , "duration" => 120 , "number_participants" => 15 , "price" => 150 , "photo" => "images/museum.jpg"}
   ]
 
   erb :index
+end
+
+get "/activities/1" do
+  @activity = {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Brive" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "/images/escape.jpg"}
+  erb :show
 end
