0547b598857a2ddc642c84c78982d5bc969df811
Cassandre <cassandreporretti@gmail.com>
2019-11-07 11:17:35 +0000

    add first answers

--- 04-databases/02-sql-queries/queries.sql
+++ 04-databases/02-sql-queries/queries.sql
@@ -4,20 +4,21 @@
 
 -- Get all sites
 -- TODO: SELECT ...
-
+select * from sites
 -- Get all sites sorted by name
 -- TODO: SELECT ...
-
--- Gett all activity names sorted by name
+select * from sites order by name
+-- Get all activity names sorted by name
 -- TODO: SELECT ...
-
+select * from activities order by name
 -- Get all "Sport" activities
 -- TODO: SELECT ...
 -- Expected result: 8 rows
-
+select * from activities where category = 'Sport'
 -- Get all Escape Games in Nantes
 -- TODO: SELECT ...
 -- Expected result: 3 rows
+select * from activities where name like '%Escape Game%'
 
 ---------------------------------------
 -- [AGGREGATES]
@@ -26,11 +27,11 @@
 -- Count all the activities
 -- TODO: SELECT ...
 -- Expected result: 44
-
+select count(*) from activities
 -- Count all "Adventure" activities
 -- TODO: SELECT ...
 -- Expected result: 13
-
+select count(*) from activities where category = 'Adventure'
 -- Count the number of activities per category
 -- TODO: SELECT ...
 -- Expected result:
@@ -42,7 +43,7 @@
 -- Sport       8
 -- Visits      5
 -- Workshops   6
-
+select category, count(*) as total  from activities group by category
 -- Get the 3 category names with the biggest number of activities
 -- TODO: SELECT ...
 -- Expected result:
@@ -51,7 +52,7 @@
 -- Adventure   13
 -- Sport       8
 -- Cultural    6
-
+select category, count(*) as total  from activities group by category order by total desc limit 3
 ---------------------------------------
 -- [JOINS]
 ---------------------------------------
@@ -59,11 +60,19 @@
 -- Get all employee names working FROM site "Ile de Nantes", sorted by first name
 -- TODO: SELECT ...
 -- Expected result: 53 rows
-
+select first_name, last_name from employees
+join sites on sites.id = employees.site_id
+where sites.name = 'Ile de Nantes'
+order by first_name
 -- Get all the activity names where you have been to, sorted by name
 -- TODO: SELECT ...
 -- Expected result: well, it depends on who you are :)
-
+select name from activities
+join team_building_sessions on team_building_sessions.activity_id = activities.id
+join participations on participations.team_building_session_id = team_building_sessions.id
+join employees on employees.id = participations.employee_id
+where employees.first_name = 'Cassandre'
+order by name
 -- [NEW KEYWORD] Get all the team names that have done an Adventure activity, sorted by names
 -- TODO: SELECT ...
 -- Expected result:
