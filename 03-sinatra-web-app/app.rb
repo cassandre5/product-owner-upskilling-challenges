66ea5843b99699603796339bbc0995504160fb94
Cassandre <cassandreporretti@gmail.com>
2019-11-07 09:41:09 +0000

    add website

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,7 +6,9 @@ enable :static
 
 get "/" do
   @activities = [
-    # TODO: gather a few activities
+    {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Paris" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "images/escape.jpg"} ,
+    {"name" => "Ping-pong" , "category" => "Sport" , "city" => "Lille" , "duration" => 90 , "number_participants" => 4 , "price" => 80 , "photo" => "images/ping-pong.jpg"},
+    {"name" => "Museum" , "category" => "Visits" , "city" => "Paris" , "duration" => 120 , "number_participants" => 15 , "price" => 150 , "photo" => "images/museum.jpg"}
   ]
 
   erb :index
