f18df600f8fde2449e3109f8e6288238d6f7bc49
Cassandre <cassandreporretti@gmail.com>
2019-11-08 15:17:24 +0000

    add dynamic activity page

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,15 +6,28 @@ enable :static
 
 get "/" do
   @activities = [
-    {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Brive" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "images/escape.jpg"} ,
-    {"name" => "Beach volley" , "category" => "Sport" , "city" => "Marseille" , "duration" => 90 , "number_participants" => 4 , "price" => 60 , "photo" => "images/beach-volley.jpg"},
-    {"name" => "Museum" , "category" => "Visits" , "city" => "Paris" , "duration" => 120 , "number_participants" => 15 , "price" => 150 , "photo" => "images/museum.jpg"}
+    {"id" => 1 , "name" => "Escape Game" , "category" => "Adventure" , "city" => "Brive" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "/images/escape.jpg"} ,
+    {"id" => 2 , "name" => "Beach volley" , "category" => "Sport" , "city" => "Marseille" , "duration" => 90 , "number_participants" => 4 , "price" => 60 , "photo" => "/images/beach-volley.jpg"},
+    {"id" => 3 , "name" => "Museum" , "category" => "Visits" , "city" => "Paris" , "duration" => 120 , "number_participants" => 15 , "price" => 150 , "photo" => "/images/museum.jpg"}
   ]
 
   erb :index
 end
 
-get "/activities/1" do
-  @activity = {"name" => "Escape Game" , "category" => "Adventure" , "city" => "Brive" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "/images/escape.jpg"}
+get "/activities/:id" do
+  p params
+  activities = [
+    {"id" => 1 , "name" => "Escape Game" , "category" => "Adventure" , "city" => "Brive" , "duration" => 60 , "number_participants" => 6 , "price" => 120 , "photo" => "/images/escape.jpg"} ,
+    {"id" => 2 , "name" => "Beach volley" , "category" => "Sport" , "city" => "Marseille" , "duration" => 90 , "number_participants" => 4 , "price" => 60 , "photo" => "/images/beach-volley.jpg"},
+    {"id" => 3 , "name" => "Museum" , "category" => "Visits" , "city" => "Paris" , "duration" => 120 , "number_participants" => 15 , "price" => 150 , "photo" => "/images/museum.jpg"}
+  ]
+id=0
+  activities.each_with_index do |activity,index|
+    if activity["id"]==params["id"].to_i
+      id=index
+    end
+  end
+puts id
+  @activity = activities[id]
   erb :show
 end
